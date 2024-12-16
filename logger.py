2fe57f0ced190dce4b7c2ab9187e666abd5cb299
Harshvardhan1407 <sharmaharshvardhan1407@gmail.com>
2024-12-16 18:49:00 +0530

    comment  streamhandler

--- logger.py
+++ logger.py
@@ -10,21 +10,21 @@ formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s:%(line
 
 # Add a RotatingFileHandler to manage log file size
 rotating_handler = RotatingFileHandler(
-    'load_forecasting.log',  # Log file name
+    'logs/load_forecasting.log',  # Log file name
     maxBytes = 10 * 1024 * 1024,  # Maximum file size: 10 MB
     backupCount = 5   # Keep 3 backup log files
 )
 rotating_handler.setFormatter(formatter)
 rotating_handler.setLevel(logging.DEBUG)  # File handler captures all messages
 
-# Add a basic StreamHandler to log messages to the console
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(formatter)
-console_handler.setLevel(logging.INFO)  # Console handler captures INFO and above
+# # Add a basic StreamHandler to log messages to the console
+# console_handler = logging.StreamHandler()
+# console_handler.setFormatter(formatter)
+# console_handler.setLevel(logging.INFO)  # Console handler captures INFO and above
 
 # Add handlers to the logger
 logger.addHandler(rotating_handler)
-logger.addHandler(console_handler)
+# logger.addHandler(console_handler)
 
 # Test the logger
 if __name__ == "__main__":
